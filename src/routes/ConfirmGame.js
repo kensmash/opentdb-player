89acbd4fc5d15ec67ccf8c1adfa78dedd6fec1db
Ken <kensmash@gmail.com>
2019-07-16 16:29:08 -0400

    handle expired token

--- src/routes/ConfirmGame.js
+++ src/routes/ConfirmGame.js
@@ -7,16 +7,28 @@ import GameContext from "../context";
 export default function ConfirmGame(props) {
   const { state, dispatch } = useContext(GameContext);
 
+  const clearTokenHandler = async () => {
+    localStorage.removeItem("apiToken");
+    const newToken = await axios.get(
+      "https://opentdb.com/api_token.php?command=request"
+    );
+    dispatch({
+      type: "GET_TOKEN",
+      payload: newToken.data.token
+    });
+    startGameHandler();
+  };
+
   const startGameHandler = async () => {
     //fetch questions
     dispatch({ type: "IS_LOADING" });
     let request = `https://opentdb.com/api.php?amount=7&category=${
       state.selectedCategory.id
-    }`;
+    }&token=${state.apiToken}`;
     if (state.selectedDifficulty !== "any") {
       request = `https://opentdb.com/api.php?amount=7&category=${
         state.selectedCategory.id
-      }&difficulty=${state.selectedDifficulty}`;
+      }&difficulty=${state.selectedDifficulty}&token=${state.apiToken}`;
     }
 
     try {
@@ -65,8 +77,15 @@ export default function ConfirmGame(props) {
         } else {
           //we had an error from the api
           console.log("oops", response.data.response_code);
+
           dispatch({ type: "LOADING_SUCCESS" });
           //TODO: deal with error somehow
+          if (
+            response.data.response_code == 3 ||
+            response.data.response_code == 4
+          ) {
+            clearTokenHandler();
+          }
         }
       });
     } catch (error) {
